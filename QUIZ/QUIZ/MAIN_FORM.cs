529c8d2e3379321f9901d0ebef6e9aa152b61274
Affne0815 <mhz_1406@web.de>
2017-02-22 15:36:04 +0100

    Add files via upload

---
+++ QUIZ/QUIZ/MAIN_FORM.cs
@@ -0,0 +1,22 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: user
+ * Date: 22.02.2017
+ * Time: 15:22
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace QUIZ
+{
+	/// <summary>
+	/// Description of Class1.
+	/// </summary>
+	public class Class1
+	{
+		public Class1()
+		{
+		}
+	}
+}
---
+++ QUIZ/QUIZ/MainForm.cs
@@ -0,0 +1,33 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: user
+ * Date: 22.02.2017
+ * Time: 15:19
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace QUIZ
+{
+	/// <summary>
+	/// Description of MainForm.
+	/// </summary>
+	public partial class MainForm : Form
+	{
+		public MainForm()
+		{
+			//
+			// The InitializeComponent() call is required for Windows Forms designer support.
+			//
+			InitializeComponent();
+			
+			//
+			// TODO: Add constructor code after the InitializeComponent() call.
+			//
+		}
+	}
+}
